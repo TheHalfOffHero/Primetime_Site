5b940a0bfc99e149ff985011d83f901c1d2a58f9
thebaron03 <mfruiz33@gmail.com>
2021-09-16 21:35:21 -0400

    started model for writeups

--- PrimeTime/writeup/models.py
+++ PrimeTime/writeup/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class post(models.Model):
+    #Fields of the model posts, I think I need a date, title, and
+    #a field for the body of the text at minimum. Subject to Change.
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    
