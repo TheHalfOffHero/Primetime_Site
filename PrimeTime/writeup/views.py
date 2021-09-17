7c2849b7339a4137ed095a6d4687ece3f1c8e324
thebaron03 <mfruiz33@gmail.com>
2021-09-17 19:34:56 -0400

    setup urls.py for writeup app, and outlined and tested very basic form of the post.

--- PrimeTime/writeup/views.py
+++ PrimeTime/writeup/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def writeup_view(request):
+    return HttpResponse('<h1>This is a test</h1>')
