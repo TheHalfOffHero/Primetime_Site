9d3abcb40a08265d65f0a46ca99a5951496c2181
thebaron03 <mfruiz33@gmail.com>
2021-09-17 20:18:48 -0400

    made a more complete post model for better manipulation/display

--- PrimeTime/writeup/models.py
+++ PrimeTime/writeup/models.py
@@ -1,9 +1,25 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import related
+
+STATUS = (
+    (0,"Draft"),
+    (1, "Publish")
+)
 
 # Create your models here.
 class post(models.Model):
-    #Fields of the model posts, I think I need a date, title, and
-    #a field for the body of the text at minimum. Subject to Change.
-    title = models.CharField(max_length=200)
-    body = models.TextField()
-    
+    #Fields of the model posts
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete= models.CASCADE, related_name='Forum_Post')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now = True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
